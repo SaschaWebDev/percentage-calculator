29ebc9416468c1cf4ba75082d56317a039dc1694
Sascha Majewsky <sascha.majewsky@pm.me>
2019-12-08 07:51:31 +0100

    improved the ui with a rounding opt out switch and created parts of the math for the functions

--- js/main.js
+++ js/main.js
@@ -1,137 +1,133 @@
 // DOM Elements
-const resultEl = document.getElementById('result');
-const lengthEl = document.getElementById('length');
-const uppercaseEl = document.getElementById('uppercase');
-const lowercaseEl = document.getElementById('lowercase');
-const numbersEl = document.getElementById('numbers');
-const symbolsEl = document.getElementById('symbols');
-const generateEl = document.getElementById('generate');
-const clipboardEl = document.getElementById('clipboard');
-const pwlengthEl = document.getElementById('pwlength');
+const methodFromFirstInput = document.getElementById('from-first-input');
+const methodFromSecondInput = document.getElementById('from-second-input');
+const methodFromResultInput = document.getElementById('from-result-input');
 
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
-};
+const methodOfFirstInput = document.getElementById('of-first-input');
+const methodOfSecondInput = document.getElementById('of-second-input');
+const methodOfResultInput = document.getElementById('of-result-input');
 
-window.onload = () => {
-  // String to number with + prefix
-  const length = +lengthEl.value;
-  const hasUpper = uppercaseEl.checked;
-  const hasLower = lowercaseEl.checked;
-  const hasNumber = numbersEl.checked;
-  const hasSymbol = symbolsEl.checked;
+const methodIncreaseFirstInput = document.getElementById('increase-first-input');
+const methodIncreaseSecondInput = document.getElementById('increase-second-input');
+const methodIncreaseResultInput = document.getElementById('increase-result-input');
 
-  resultEl.innerText = generatePassword(
-    hasUpper,
-    hasLower,
-    hasNumber,
-    hasSymbol,
-    length,
-  );
-};
+const buttonConvert = document.getElementById('button-convert');
+const switchRounding = document.getElementById('switch-rounding');
+const switchTheme = document.getElementById('switch-theme');
 
-// Gnerate event listen on click
-generateEl.addEventListener('click', () => {
-  // String to number with + prefix
-  const length = +lengthEl.value;
-  const hasUpper = uppercaseEl.checked;
-  const hasLower = lowercaseEl.checked;
-  const hasNumber = numbersEl.checked;
-  const hasSymbol = symbolsEl.checked;
 
-  resultEl.innerText = generatePassword(
-    hasUpper,
-    hasLower,
-    hasNumber,
-    hasSymbol,
-    length,
-  );
-});
+// CALCULATE RESULTS
 
-lengthEl.addEventListener('input', () => {
-  pwlengthEl.innerText = +lengthEl.value;
+// Calculate FROM
+methodFromFirstInput.addEventListener('input', () => {
+  const firstInput = methodFromFirstInput.value;
+  const secondInput = methodFromSecondInput.value;
+  const rounding = switchRounding.checked;
 
-  const length = +lengthEl.value;
-  const hasUpper = uppercaseEl.checked;
-  const hasLower = lowercaseEl.checked;
-  const hasNumber = numbersEl.checked;
-  const hasSymbol = symbolsEl.checked;
-
-  resultEl.innerText = generatePassword(
-    hasUpper,
-    hasLower,
-    hasNumber,
-    hasSymbol,
-    length,
-  );
-});
-
-// Generate password functions
-function generatePassword(upper, lower, number, symbol, length) {
-  let generatedPassword = '';
-  const typesCount = upper + lower + number + symbol;
-
-  const typesArr = [{ upper }, { lower }, { number }, { symbol }].filter(
-    element => Object.values(element)[0],
-  );
-
-  return typesCount === 0
-    ? 'what did you expect?'
-    : generation(typesCount, typesArr, length, generatedPassword).slice(
-        0,
-        length,
-      );
-}
-
-function generation(typesCount, typesArr, length, generatedPassword) {
-  for (let i = 0; i < length; i += typesCount) {
-    typesArr.forEach(type => {
-      const funcName = Object.keys(type)[0];
-      generatedPassword += randomFunc[funcName]();
-    });
-  }
-  return generatedPassword;
-}
-
-// Copy password to clip
-clipboardEl.addEventListener('click', () => {
-  const textarea = document.createElement('textarea');
-  const password = resultEl.innerText;
-
-  if (!password) {
+  if (!firstInput || !secondInput) {
+    alert('EMPTY')
     return;
   } else {
-    textarea.value = password;
+    rounding ? methodFromResultInput.value = methodFromResultInput.value = roundNumber(((secondInput * (firstInput / 100)) / 100) * 100, 2) : ((secondInput * (firstInput / 100)) / 100) * 100;
+  }
+});
+
+methodFromSecondInput.addEventListener('input', () => {
+  const firstInput = methodFromFirstInput.value;
+  const secondInput = methodFromSecondInput.value;
+  const rounding = switchRounding.checked;
+
+  if (!firstInput || !secondInput) {
+    alert('EMPTY')
+    return;
+  } else {
+    rounding ? methodFromResultInput.value = methodFromResultInput.value = roundNumber(((secondInput * (firstInput / 100)) / 100) * 100, 2) : ((secondInput * (firstInput / 100)) / 100) * 100;
+  }
+});
+
+
+
+
+
+
+
+ // MATH FUNCTIONS
+
+ // ROUNDING
+function roundNumber(num, scale) {
+  if(!("" + num).includes("e")) {
+    return +(Math.round(num + "e+" + scale)  + "e-" + scale);
+  } else {
+    var arr = ("" + num).split("e");
+    var sig = ""
+    if(+arr[1] + scale > 0) {
+      sig = "+";
+    }
+    return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) + "e-" + scale);
+  }
+}
+
+
+switchRounding.addEventListener('click', () => {
+  const rounding = switchRounding.checked;
+
+  if (rounding) {
+    if (methodFromFirstInput.value && methodFromSecondInput.value) {
+      methodFromResultInput.value = roundNumber(((methodFromSecondInput.value * (methodFromFirstInput.value / 100)) / 100) * 100, 2);
+    }
+  } else {
+    return;
+  }
+});
+
+
+
+// COPY TO CLIPBOARD
+
+// Copy result of FROM method to clipboard
+methodFromResultInput.addEventListener('click', () => {
+  const textarea = document.createElement('textarea');
+  const result = methodFromResultInput.value;
+
+  if (!result) {
+    return;
+  } else {
+    textarea.value = result;
     document.body.appendChild(textarea);
     textarea.select();
     document.execCommand('copy');
     textarea.remove();
-    alert('Password copied to clipboard');
   }
 });
 
-// Generator functions
-// See chars at http://net-comber.com/charset.html String.fromCharCode(97)
+// Copy result of OF method to clipboard
+methodOfResultInput.addEventListener('click', () => {
+  const textarea = document.createElement('textarea');
+  const result = methodOfResultInput.value;
 
-function getRandomLower() {
-  // Chars needs to be within 97-122 (a-z)
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+  if (!result) {
+    return;
+  } else {
+    textarea.value = result;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+  }
+});
 
-function getRandomUpper() {
-  // Chars needs to be within 65-90 (A-Z)
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+// Copy result of INCREASE method to clipboard
+methodIncreaseResultInput.addEventListener('click', () => {
+  const textarea = document.createElement('textarea');
+  const result = methodIncreaseResultInput.value;
 
-function getRandomNumber() {
-  // Chars needs to be within 48-57 (0-10)
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-  const symbols = '!@#$%^&*(){}[]=<>/,.';
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
+  if (!result) {
+    return;
+  } else {
+    textarea.value = result;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+  }
+});
